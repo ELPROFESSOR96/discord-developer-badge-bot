b3cde18ef196d8ed4bb1517d06b1f1721df93c9e
Happpydust <117503464+Happpydust@users.noreply.github.com>
2023-07-28 13:48:27 +1000

    Update install-auto.sh

--- install-auto.sh
+++ install-auto.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git https://github.com/Happpydust/discord-developer-badge-bot.git
+git clone https://github.com/Happpydust/discord-developer-badge-bot.git
 cd discord-developer-badge-bot
-pip3 install -r requirements.txt
+pip3 install nextcord
+python3 main.py
